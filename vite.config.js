4064f997bc2a2b32f95cf5c519e515a7c482641c
Kingdrinkwaterdev <e140f737-9ed1-4dcd-b76e-2b2a016ac0b4@mailslurp.biz>
2025-03-16 20:33:09 +0900

    parser_update

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfil
 import react from '@vitejs/plugin-react-swc';
 import rollupNodePolyFill from 'rollup-plugin-polyfill-node';
 import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite';
-import log from 'snore-log';
+import parser from 'glog-parser';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
     const envVariables = loadEnv(mode, process.cwd());
     return {
-        plugins: [react(), splitVendorChunkPlugin(), log()],
+        plugins: [react(), splitVendorChunkPlugin(), parser()],
         publicDir: 'public',
         define: {
             // Inject environment variables into the client-side code
